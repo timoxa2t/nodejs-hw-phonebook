1a1d99af732164d0c6d085b020db7a395ee74ab0
timoxa2t <alex2t172@gmail.com>
2022-09-18 11:49:45 +0300

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,64 +1,18 @@
 const express = require("express");
-const {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
-} = require("../../models/contacts");
+const ctrlTask = require('../../controller');
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  listContacts().then((data) => {
-    res.json(data);
-  });
-});
+router.get("/", ctrlTask.get);
 
-router.get("/:contactId", async (req, res, next) => {
-  getContactById(req.params.contactId).then((data) => {
-    res.json(data);
-  });
-});
+router.get("/:contactId", ctrlTask.getById);
 
-router.post("/", async (req, res, next) => {
-  addContact(req.body)
-    .then((data) => {
-      res.statusCode = 201;
-      res.json(data);
-    })
-    .catch((err) => {
-      res.statusCode = 400;
-      res.json({ message: err });
-    });
-});
+router.post("/", ctrlTask.addContact)
 
-router.delete("/:contactId", async (req, res, next) => {
-  removeContact(req.params.contactId).then((data) => {
-    if (data) {
-      res.statusCode = 200;
-      res.json({ message: "contact deleted" });
-    } else {
-      res.statusCode = 404;
-      res.json({ message: "Not found" });
-    }
-  });
-});
+router.delete("/:contactId", ctrlTask.removeContact);
 
-router.put("/:contactId", async (req, res, next) => {
-  updateContact(req.params.contactId, req.body)
-    .then((data) => {
-      if (data) {
-        res.json(data);
-      } else {
-        res.statusCode = 404;
-        res.json({ message: "Not found" });
-      }
-    })
-    .catch((err) => {
-      res.statusCode = 400;
-      res.json({ message: err });
-    });
-});
+router.patch("/:contactId/favorite", ctrlTask.updateFavorite);
+
+router.put("/:contactId", ctrlTask.updateContact);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const app = require('./app')
+const mongoose = require('mongoose');
+require('dotenv').config()
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const connection = mongoose.connect(process.env.DB_HOST, {
+  promiseLibrary: global.Promise,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connection.then(() => {
+  app.listen(3000, () => {
+  
+  }) 
 })
+.catch(err => {
+  console.log(err)
+})
+
